3c601422b6327d53716c648371c39c54871cd72d
G2-Rocha <132342030+G2-Rocha@users.noreply.github.com>
2023-10-25 00:36:31 +0100

    Revert "fix fix"
    This reverts commit 9a6a2409b8a00cf7757120f03a0abb0e6e262107.

--- pages/register.js
+++ pages/register.js
@@ -9,9 +9,11 @@ import axios from 'axios';
 import fetch from 'isomorphic-unfetch';
 import io from 'socket.io-client';
 
+const port = parseInt(process.env.PORT, 10) || 3000;
+
 export default function Home({}) {
   useEffect(() => {
-    const socket = socketIOClient(`https://king-prawn-app-9ucth.ondigitalocean.app`);
+    const socket = io(`https://king-prawn-app-9ucth.ondigitalocean.app`);
 
     socket.on('event', (data) => {
       console.log('Event received:', data);
