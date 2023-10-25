091aedaa03beba6a880513790dc5017c7c36ef3c
G2-Rocha <132342030+G2-Rocha@users.noreply.github.com>
2023-10-25 12:43:06 +0100

    fdsfdsf

--- pages/api/events.js
+++ pages/api/events.js
@@ -1,22 +1,22 @@
-import { getIo } from '../../util/socket.js';
-const express = require('express');
-const next = require('next');
-const http = require('http');
-
-const server = express();
-const httpServer = http.createServer(server);
-
-// Integrate socket.js here
+import { connectToDatabase } from '../../util/mongodb';
+import { Server } from 'socket.io';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    // Handle the webhook event here
     console.log('Webhook received:', req.body);
-    const io = require('../../util/socket.js')(httpServer);
 
+    // You might want to do something with the database here
+    // const { db } = await connectToDatabase();
+    // ... database operations ...
+    const io = new Server(res.socket.server);
     io.emit('event', req.body);
     res.status(200).json({ message: 'Webhook received' });
+
+
   } else {
+    // Handle any other HTTP method
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
   }
-};
+}
