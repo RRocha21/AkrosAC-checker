51114c08cad01500790184e68aa9030daec783fd
G2-Rocha <132342030+G2-Rocha@users.noreply.github.com>
2023-10-25 11:27:19 +0100

    sdfsdf

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  httpServer.listen(3000, (err) => {
+  httpServer.listen(8080, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log('> Ready on http://localhost:8080');
   });
 });
