2c7bf62c9b5ed1fe598b25b33cff6096923bdd29
G2-Rocha <132342030+G2-Rocha@users.noreply.github.com>
2023-10-25 13:23:00 +0100

    dfdfdff

--- pages/register.js
+++ pages/register.js
@@ -11,16 +11,47 @@ import io from 'socket.io-client';
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 
+async function getSteamUsername(steamUID) {
+  const apiKey = '8D84EBFF5DCB6429F357949D448F406F';
+  const url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamUID}`;
+
+  try {
+    const response = await axios.get(url);
+    const players = response.data.response.players;
+    if (players.length > 0) {
+      const username = players[0].personaname;
+      console.log('Steam Username:', username);
+      return username;
+    } else {
+      console.log('User not found');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error retrieving Steam username:', error);
+    return null;
+  }
+}
+
 export default function Home({}) {
+  const [events, setEvents] = useState([]);
   useEffect(() => {
     const socket = io(`https://king-prawn-app-9ucth.ondigitalocean.app`);
     socket.connect()
 
-    socket.on('event', (data) => {
+    socket.on('event', async (data) => {
       console.log('Event received:', data);
       // Here you can set the state or call functions in your component
+      const username = await getSteamUsername('76561198262506391');
+      if (data.data?.userId) {
+        console.log('User ID:', data.data.userId)
+        const username = getSteamUsername(data.data.userId);
+        console.log('Username:', username);
+      }
     });
     
+    // if ()
+
+
     return () => {
       socket.disconnect();
     };
