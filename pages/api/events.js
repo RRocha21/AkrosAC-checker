bc3aede0f67e1de919950329dda0d84ec37bd033
G2-Rocha <132342030+G2-Rocha@users.noreply.github.com>
2023-10-25 12:26:11 +0100

    dsfsdf

--- pages/api/events.js
+++ pages/api/events.js
@@ -7,7 +7,7 @@ const httpServer = http.createServer(server);
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     console.log('Webhook received:', req.body);
-    const io = require('./util/socket.js')(httpServer);
+    const io = require('../../util/socket.js')(httpServer);
 
     io.emit('event', req.body);
     res.status(200).json({ message: 'Webhook received' });
