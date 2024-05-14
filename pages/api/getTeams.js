8aaeb2c9163d766511de6c4a65f118e557b03299
G2-Rocha <132342030+G2-Rocha@users.noreply.github.com>
2024-05-14 11:08:35 +0100

    fix

--- pages/api/getTeams.js
+++ pages/api/getTeams.js
@@ -51,13 +51,12 @@ export default async function handler(req, res) {
 }
 
 async function authorize() {
-    const credentialsPath = path.resolve(__dirname, '../../../../credentials.json');
-    console.log('Attempting to load credentials from:', credentialsPath);
-    
+    const credentialsPath = path.resolve(__dirname, '../credentials.json');
+  
     const auth = new google.auth.GoogleAuth({
         keyFile: credentialsPath, // Now using the correct path
         scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
     });
-
+    console.log('auth', auth)
     return await auth.getClient();
 }
